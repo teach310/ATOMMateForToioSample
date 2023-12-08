43c0dbbffe6c12be83ec0a90ded523a00cdb897e
teach310 <okms310@yahoo.co.jp>
2023-12-08 23:46:20 +0900

    Update ToioMoveAway.cs with improved tolerance and speed calculation

--- Assets/Scripts/ToioBehaviours/ToioMoveAway.cs
+++ Assets/Scripts/ToioBehaviours/ToioMoveAway.cs
@@ -5,11 +5,11 @@ public class ToioMoveAway : ToioBehaviour
 {
     Vector2 targetPos;
     public float MaxSpeed = 100f;
-    public int Tolerance = 30; // 許容範囲　default 8。大きめにすることで目標地点近辺で止まらないようにする
+    public int Tolerance = 15; // 許容範囲　default 8。大きめにすることで目標地点近辺で止まらないようにする
     public int RotateTime = 140; // default 250 切り返し早めるために回転速度を早める
     float _maxTime = 2f;
     float _enterTime;
-    
+
     public override void OnEnter()
     {
         base.OnEnter();
@@ -37,7 +37,7 @@ public class ToioMoveAway : ToioBehaviour
         // CubeOrderBalancerのsendQueueに2つ以上ためっているというログを回避するためにIsIdleを確認
         if (cubeManager.synced && CubeOrderBalancer.Instance.IsIdle(cube))
         {
-            Movement mv = cubeHandle.Move2Target(targetPos, maxSpd: MaxSpeed, rotateTime: RotateTime, tolerance: Tolerance).Exec();
+            Movement mv = cubeHandle.Move2Target(targetPos, maxSpd: CalcSpeed(), rotateTime: RotateTime, tolerance: Tolerance).Exec();
             if (mv.reached)
             {
                 face.SetExpression(ToioFace.Expression.Happy);
@@ -59,24 +59,39 @@ public class ToioMoveAway : ToioBehaviour
         var forward = new Vector2((float)cubeHandle.dir.x, (float)cubeHandle.dir.y);
         var borderRect = cubeHandle.borderRect;
 
-        float dot = 1f;
+        float dot = float.MaxValue;
         Vector2 resultPos = Vector2.zero;
         // 数回試行して、一番逆向きの方向を選択
-        for (int i = 0; i < 6; i++)
+        int counter = 0;
+        while (dot > 0f && counter < 10)
         {
-            var tempPos = new Vector2(
-                Random.Range(borderRect.xMin, borderRect.xMax),
-                Random.Range(borderRect.yMin, borderRect.yMax)
-            );
-
-            var tempDot = Vector2.Dot(forward, tempPos);
-
-            if (tempDot < dot)
+            for (int i = 0; i < 10; i++)
             {
-                dot = tempDot;
-                resultPos = tempPos;
+                var tempPos = new Vector2(
+                    Random.Range(borderRect.xMin, borderRect.xMax),
+                    Random.Range(borderRect.yMin, borderRect.yMax)
+                );
+
+                var tempDot = Vector2.Dot(forward, tempPos);
+
+                if (tempDot < dot)
+                {
+                    dot = tempDot;
+                    resultPos = tempPos;
+                }
             }
+            counter++;
         }
+
         targetPos = resultPos;
+    }
+
+    public float CalcSpeed()
+    {
+        // Slow down as cube get closer to the destination.
+        var cubePos = new Vector2((float)cubeHandle.x, (float)cubeHandle.y);
+        var distance = Vector2.Distance(cubePos, targetPos);
+        float coeff = Mathf.SmoothStep(0.6f, 1f, distance / 30f);
+        return MaxSpeed * coeff;
     }
 }
